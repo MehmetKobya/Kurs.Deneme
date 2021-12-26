d6920df67a86b174ec17769ab472765ab59eff3f
Mehmet <mehmet@vidarti.com>
2021-12-26 12:52:56 +0300

    Gizeme hava atma

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require ("http");
 const requestListener = function (req,res){
     console.log(req)
     res.writeHead(200);
-    res.end("Hello");
+    res.end("Hello world's");
 }
 const server = http.createServer(requestListener);
 server.listen(8080);
